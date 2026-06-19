026461e2b8f00ade703488d673cc7a3df788710a
Deepsingh77142 <deepsingh77142@gmail.com>
2026-06-19 13:39:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
-import { useEffect } from 'react';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.style.background = '#0a0a0f';
-    document.body.style.background = '#0a0a0f';
-    document.body.style.margin = '0';
-    document.body.style.padding = '0';
-  }, []);
-
   return <Component {...pageProps} />;
 }
