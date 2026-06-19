ec04bd95901a3d16d3c13757de932973a4f1812e
Deepsingh77142 <deepsingh77142@gmail.com>
2026-06-19 13:27:14 +0530

    Update _app.js

--- pages/pages/_app.js
+++ pages/pages/_app.js
@@ -1,17 +1,1 @@
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <style jsx global>{`
-        html, body {
-          background: #0a0a0f;
-          margin: 0;
-          padding: 0;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  );
-}
+
